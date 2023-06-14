e9f9eee09e6a90e50afba7242da6de1500ecf004
mixayam <myastrebtsov@gmail.com>
2023-06-14 21:12:52 +0300

    KS-1-2 first prog was made

---
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -0,0 +1,11 @@
+package lesson_1
+
+fun main(){
+        val numOfOrders = 75
+        val numOfWorkers = 1999
+        val greeting = "Thanks for ordering!"
+        println(numOfOrders)
+        println(greeting)
+        //println(numOfWorkers)
+        println(numOfWorkers)
+}
