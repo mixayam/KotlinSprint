4fa4e8c1d6471d2223842eb0e719e77b5d59c7cd
mixayam <myastrebtsov@gmail.com>
2023-06-15 21:28:26 +0300

    KS-1-3 задание выполнено

---
+++ src/main/kotlin/lesson_1/lesson_1task_1.kt
@@ -0,0 +1,15 @@
+package lesson_1
+
+fun main(){
+    val earOfFlight = 1961
+    var hour = 9
+    var minute = 7
+    hour = 10
+    minute = 55
+    println(earOfFlight)
+    //println(hour)
+    //println(minute)
+    print(hour)
+    print(":")
+    print(minute)
+}
