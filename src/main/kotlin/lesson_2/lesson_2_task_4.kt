4ee16095fda6e97f5580449172f024c568cdec2b
mixayam <myastrebtsov@gmail.com>
2023-06-18 11:49:17 +0300

    KS-2-4 задание выполнено

---
+++ src/main/kotlin/lesson_2/lesson_2_task_4.kt
@@ -0,0 +1,12 @@
+package lesson_2
+
+fun main(){
+    val numOfCristal = 7
+    val numOfIron = 11
+    val buffForCristal:Int = (numOfCristal * 20) / 100
+    val buffForIron:Int = (numOfIron * 20) / 100
+    println("buff for cristals: $buffForCristal ")
+    println("buff for iron: $buffForIron")
+}
+
+
