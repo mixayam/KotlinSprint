053530acfda927a5112c9a285860a7175082557d
mixayam <myastrebtsov@gmail.com>
2023-06-18 11:22:24 +0300

    KS-2-3 задание выполнено

---
+++ src/main/kotlin/lesson_2/lesson_2_task_3.kt
@@ -0,0 +1,24 @@
+package lesson_2
+
+fun main(){
+    //Способ 1, подходит для данных в конкретной задаче
+    val departureTimeHours = 9 //час отправления
+    val departureTimeMins = 39 //минуты отправления
+    val tripTimeMinsSum = 457 //сумма минут в пути
+    val tripTimeHours = tripTimeMinsSum / 60 //часы в пути
+    val tripTimeMins = tripTimeMinsSum - (tripTimeHours * 60) //минуты в пути
+    val arrivalTimeMins = (departureTimeMins + tripTimeMins) - 60 //минуты прибытия
+    val arrivalTimeHours = departureTimeHours + tripTimeHours + 1 //часы прибытия
+
+    println(arrivalTimeHours)
+    println(arrivalTimeMins)
+
+
+    //Способ 2, подходит для любых значений времени
+    /*if (arrivalTimeMins > 60){
+        arrivalTimeMins = (departureTimeMins + tripTimeMins) - 60
+        val arrivalTimeHours = departureTimeHours + tripTimeHours + 1
+    }
+    println(arrivalTimeHours)
+    println(arrivalTimeMins)*/
+}
