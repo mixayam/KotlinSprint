1cd4d4b66a32967b34d27f5355771f9065c968fb
mixayam <myastrebtsov@gmail.com>
2023-06-20 07:42:06 +0300

    KS-2-3 исправлены ошибки

--- src/main/kotlin/lesson_2/lesson_2_task_3.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_3.kt
@@ -9,17 +9,7 @@ fun main(){
     val arrivalTimeMinsSum  = departureTimeMins + tripTimeMinsSum
     val arrivalTimeHours = departureTimeHours + tripTimeHours
     val arrivalTimeMins = arrivalTimeMinsSum % 60
-
-
-    /*val tripTimeMins = tripTimeMinsSum - (tripTimeHours * 60) //минуты в пути
-    val arrivalTimeMins = (departureTimeMins + tripTimeMins) - 60 //минуты прибытия
-    val arrivalTimeHours = departureTimeHours + tripTimeHours + 1 //часы прибытия*/
-
-
-
     println("$arrivalTimeHours:$arrivalTimeMins")
-    //println(arrivalTimeMins)
-
 
     //Способ 2, подходит для любых значений времени
     /*if (arrivalTimeMins > 60){
