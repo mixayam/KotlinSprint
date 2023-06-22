8e78852d11e03846bf16af5fc0d8c61e00273a94
mixayam <myastrebtsov@gmail.com>
2023-06-22 12:30:10 +0300

    KS-3-1 исправлены ошибки

--- src/main/kotlin/lesson_3/lesson_3_task_1.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_1.kt
@@ -1,13 +1,19 @@
 package lesson_3
 
+import java.time.Clock
+import java.time.LocalTime
+import java.time.ZoneId
+
 fun main(){
     val morningGreeting = "Good morning,"
     val eveningGreeting = "Good evening,"
     val userName = "User1"
-    val Greeting ="""$morningGreeting $userName 
-        |$eveningGreeting $userName
-    """.trimMargin()
-    println(Greeting)
+    val time = LocalTime.now(Clock.system(ZoneId.systemDefault())).hour
+    if(time <= 12) {
+        println(morningGreeting + userName)
+    } else {
+        println(eveningGreeting + userName)
+    }
 
 
 }
