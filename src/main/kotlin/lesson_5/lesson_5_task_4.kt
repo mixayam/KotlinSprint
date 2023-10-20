a22d987e61fc04038c58bf17170707978d05d9e2
mixayam <myastrebtsov@gmail.com>
2023-10-20 14:06:39 +0300

    KS-5-4 задание выполнено

---
+++ src/main/kotlin/lesson_5/lesson_5_task_4.kt
@@ -0,0 +1,23 @@
+package lesson_5
+
+fun main(){
+
+    println("Введите имя пользователя")
+
+    var enteredUsername = readln().toString()
+
+    if (enteredUsername == USERNAME) {
+        println("Введите пароль:")
+    } else {
+        println("Пожалуйста, зарегестрируйтесь.")
+        return
+    }
+
+    var enteredPassword = readln().toString()
+
+    if (enteredPassword == PASSWORD){
+        println("Добро пожаловать!")
+    }
+}
+const val USERNAME : String = "Zaphod"
+const val PASSWORD : String = "PanGalactic"
