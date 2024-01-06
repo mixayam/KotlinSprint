0ebd74b01f53a37783d9a6eeeb8274060966c4ba
Yas.tech <myastrebtsov@gmail.com>
2024-01-06 17:43:39 +0300

    KS-6-6 ошибки исправлены

--- src/main/kotlin/lesson_6/lesson_6_task_4.kt
+++ src/main/kotlin/lesson_6/lesson_6_task_4.kt
@@ -1,19 +1,20 @@
 package lesson_6
+
 fun main() {
-    var counter = 5
+    var counter = NUMBER
 
     while (counter > 0) {
-        println("Угадайте число")
-        val userNumber = readln().toInt()
-        println("Осталось попыток ${counter--}")
-        if (userNumber == NUMBER) {
-            println("Это была великолепная игра!")
-            return
-        } else {
-            println("неверно")
-        }
-    }
-    println("Было загадано число $NUMBER")
-}
+           println("Угадайте число")
+           val userNumber = readln().toInt()
+           if (userNumber == NUMBER) {
+           println("Это была великолепная игра!")
+               return
+           } else {
+               println("неверно")
+               println("Осталось попыток ${counter--}")
+           }
+       }
+        println("Было загадано число $NUMBER")
 
+}
 const val NUMBER = 5
